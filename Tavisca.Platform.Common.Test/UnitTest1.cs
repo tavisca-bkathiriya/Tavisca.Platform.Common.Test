c3ec8806ddc382891a7be154621b2f6f991698ce
Brij Mohan <bmohan@tavisca.in>
2020-02-27 18:25:45 +0530

    Add project files.

---
+++ Tavisca.Platform.Common.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tavisca.Platform.Common.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
