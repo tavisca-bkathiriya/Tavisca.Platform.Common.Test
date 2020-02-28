aae371ab263ad34cf07164eb3ceb8ebbd16adf1c
Brij Mohan <bmohan@tavisca.in>
2020-02-28 18:31:46 +0530

    added tests for logging web

---
+++ Tavisca.Platform.Common.Logging.Tests.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tavisca.Platform.Common.Logging.Tests.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
